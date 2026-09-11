059b53e3d7fac9a85e5dcfde733cddb0260788bb
wasu44 <szczepanlop@gmail.com>
2026-09-11 08:53:48 +0200

    DNP

---
+++ Server/RepositorContracts/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Entity;
+
+namespace RepositorContracts;
+
+public interface IUserRepository
+{
+    Task<User> AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(int id);
+    Task<User> GetSingleAsync(int id);
+    IQueryable<User> GetMany();
+}
